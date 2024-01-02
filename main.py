49975358130d4ea12f3970e9e90d0a57ffb36b3a
Sebastopaul <sebastien.lamard.pro@gmail.com>
2024-01-02 11:58:51 +0100

    feat: ferme

---
+++ main.py
@@ -0,0 +1,44 @@
+from datetime import date
+
+class Animal:
+    def __init__(self, birthDate):
+        self.birthDate = birthDate
+
+    def getAge(self):
+        return self.birthDate - date()
+        
+class Chat(Animal):
+    def pousseCri(self):
+        print('Meow')
+        
+class Vache(Animal):
+    def pousseCri(self):
+        print('Mooo')
+        
+class Poussin(Animal):
+    def pousseCri(self):
+        print('Piou')
+
+class Ferme:
+    nbAnimaux = 0
+    
+    def __init__(self):
+        self.animaux = []
+
+    def ajouteAnimal(self, animal):
+        self.animaux.append(animal)
+        self.nbAnimaux += 1
+        
+    def ecouteAnimaux(self):
+        for i in range(self.nbAnimaux):
+            self.animaux[i].pousseCri()
+    
+    def main(self):
+        self.ajouteAnimal(Chat(date(2020, 1, 2)))
+        self.ajouteAnimal(Poussin(date(2024, 1, 2)))
+        self.ajouteAnimal(Vache(date(2002, 1, 2)))
+        
+        self.ecouteAnimaux()
+
+if __name__ == '__main__':
+    Ferme().main()
